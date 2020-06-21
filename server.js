1ff910a0ceec051778fa830321ab811ea4ba29bb
panvicka <panvicka@gmail.com>
2020-06-21 20:10:54 +0200

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+var corsOptions = {
+    origin: ["http://localhost:4200", "http://localhost:4201"]
+}
+
+app.use(cors(corsOptions));
 
 app.listen(8000, () => {
 
     console.log("server is started and listening");
 
 });
+
+
+app.get("/", function (request, response) {
+    response.send("Hello lode.js");
+});
+
+
+require("./articles.js")(app);
